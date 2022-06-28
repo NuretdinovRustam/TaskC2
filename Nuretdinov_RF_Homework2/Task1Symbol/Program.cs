134ab5e740364108be5d78220136256b56735114
NuretdinovRustam <107863747+NuretdinovRustam@users.noreply.github.com>
2022-06-28 09:12:57 +0400

    1
    1

---
+++ Nuretdinov_RF_Homework2/Task1Symbol/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1Symbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символы");
+            string num = Console.ReadLine();
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i].ToString() == "x")
+                {
+                    Console.WriteLine("x");
+                    break;
+                }
+                else if (num[i].ToString() == "w")
+                {
+                    Console.WriteLine("w");
+                    break;
+                }
+            }
+
+        }
+    }
+}
