da6535d02628554272527c14f575ce20bcca9128
NuretdinovRustam <107863747+NuretdinovRustam@users.noreply.github.com>
2022-07-04 08:56:15 +0400

    1
    1

---
+++ Nuretdinov_RF_Homework2/Task2multiple7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task2multiple7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
