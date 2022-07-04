8d624ac28bf5a49f2a42af6a6fe958c62a480bd5
NuretdinovRustam <107863747+NuretdinovRustam@users.noreply.github.com>
2022-07-04 15:17:50 +0400

    4
    4 задание

---
+++ Task3SecondMax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task3SecondMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Task4YesOrNo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task4YesOrNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число");
+            string num = Console.ReadLine();
+            if (UInt32.TryParse(num, out var parsedNum))
+            {
+                if (parsedNum == 0)
+                {
+                    Console.WriteLine("Ввели некорректное значение");
+                }
+                else
+                {
+                    int check = Convert.ToInt32(num);
+                    if ((!((check % 2) == 0)) | ((check % 2) == 0 & check >= 6 & check <= 20))
+                    {
+                        Console.WriteLine("YES");
+                    }
+                    else if (((check % 2) == 0) & ((check >= 2 & check <= 5) | (check > 20)))
+                    {
+                        Console.WriteLine("NO");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ввели некорректное значение");
+            }
+        }
+    }
+}
