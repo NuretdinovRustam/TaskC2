bc68e6936c2f34ec7341acb73b6bcb0e4c0884f2
NuretdinovRustam <107863747+NuretdinovRustam@users.noreply.github.com>
2022-07-26 17:07:10 +0400

    1
    1

--- Task3SecondMax/Program.cs
+++ Task3SecondMax/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace Task3SecondMax
 {
@@ -6,7 +9,50 @@ namespace Task3SecondMax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите числа не превышаюшее по значению 1000, минимум ввести 2 числа, если число закончится на 0, то ввод чисел прекратится");
+            bool cycl = true;
+            var num1 = 1;
+            var num2 = 0;
+            var num3 = 0;
+            try
+            {
+                while (cycl == true)
+                {
+                    string num = Console.ReadLine();
+                    if (num1 > num2 & num3 == 0)
+                    {
+                        num1 = int.Parse(num);
+                        num3 = 1;
+                    }
+                    else if (num1 > num2 & num2 == 0)
+                    {
+                        num2 = int.Parse(num);
+                    }
+                    else if (num1 < num2)
+                    {
+                        num2 = int.Parse(num);
+                    }
+                    else if (num == "0")
+                    {
+                        break;
+                    }
+                }
+                if (num1 < num2)
+                {
+                    Console.WriteLine(num1);
+                }
+                else if (num2 < num1)
+                {
+                    Console.WriteLine(num2);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
         }
     }
 }
---
+++ bubble algorithm/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace bubble_algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            string bubble = Console.ReadLine();
+        }
+    }
+}
