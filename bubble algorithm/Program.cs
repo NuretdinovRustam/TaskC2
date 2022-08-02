1499c4547bad7f545e21babe43ebce1e7a58bcb0
NuretdinovRustam <107863747+NuretdinovRustam@users.noreply.github.com>
2022-08-02 13:16:43 +0400

    5
    добавил комменты

--- bubble algorithm/Program.cs
+++ bubble algorithm/Program.cs
@@ -14,10 +14,12 @@ namespace bubble_algorithm
                 Console.WriteLine("Введите числа через пробел");
                 int buffer;
                 string numbers = Console.ReadLine();
+                //создаем масив, с помощью метода Split убираем лишние пробелы
                 string[] masString = numbers.Split(' ');
                 int[] masInt = new int[masString.Length];
+                //конвертируем из string в int массив
                 masInt = Array.ConvertAll(masString, s => int.Parse(s));
-
+                //сортировка массива
                 for (int i = 0; i < masInt.Length - 1; i++)
                 {
                     for (int j = i + 1; j < masInt.Length; j++)
