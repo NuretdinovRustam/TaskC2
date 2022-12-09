296c6397fd01b050ee3b8c721c00bfcae25c3174
Рустам Нуретдинов <bart157@yandex.ru>
2022-12-09 17:33:36 +0400

    123

--- Task3SecondMax/Program.cs
+++ Task3SecondMax/Program.cs
@@ -10,25 +10,22 @@ namespace Task3SecondMax
         static void Main(string[] args)
         {            
             int max1 = 0;
-            int max2 = 0;            
-                while (true)
+            int max2 = 0;
+            int num;
+            do
+            {
+                num = int.Parse(Console.ReadLine());
+                if (num > max1)
                 {
-                    int num = int.Parse(Console.ReadLine());
-                    if (num == 0)
-                    {
-                        break;
-                    }
-                    else if (num > max1)
-                    {
-                        max2 = max1;
-                        max1 = num;
-                    }
-                    else if (num > max2)
-                    {
-                        max2 = num;
-                    }
-
+                    max2 = max1;
+                    max1 = num;
                 }
+                else if (num > max2)
+                {
+                    max2 = num;
+                }
+            }
+            while (num != 0);
             Console.WriteLine(max2);
         }
     }
